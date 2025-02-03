38430ce211876b00a6ee803b9a4588151866f29c
Aislx Flames <120901302+aislxflames@users.noreply.github.com>
2025-02-03 13:34:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,11 @@
 curl "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev" -o Lavalink.jar
 echo Lavalink.jar is downloaded...
 
-java -jar Lavalink.jar
+
+
+npm i pm2
+echo Installing pm2
+
+echo Starting Lavalink
+pm2 start java -- -jar Lavalink.jar
+echo Strted Lavalink
